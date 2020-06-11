8d326085255f0b2db52b28477a7462fc47451906
maxsteel1996 <yusufmax3@gmail.com>
2020-06-11 13:00:49 +0530

    first point

--- src/com/calculator/StringCalculator.java
+++ src/com/calculator/StringCalculator.java
@@ -2,4 +2,16 @@ package com.calculator;
 
 public class StringCalculator {
 
+	public int add(String input) {
+		if (input == null || input.isEmpty()) {
+			return 0;
+		}
+		String[] splitInputs = input.split(",");
+		int sum = 0;
+		for (String splString : splitInputs) {
+			sum += Integer.parseInt(splString);
+		}
+		return sum;
+	}
+
 }
--- src/com/calculator/test/CalculatorTest.java
+++ src/com/calculator/test/CalculatorTest.java
@@ -4,11 +4,32 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import com.calculator.StringCalculator;
+
 class CalculatorTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void addEmptyTest() {
+		StringCalculator stringCalculator = new StringCalculator();
+		String input = "";
+		int sum = stringCalculator.add(input);
+		assertEquals(sum, 0);
+	}
+
+	@Test
+	void addOneInputTest() {
+		StringCalculator stringCalculator = new StringCalculator();
+		String input = "1";
+		int sum = stringCalculator.add(input);
+		assertEquals(sum, 1);
+	}
+
+	@Test
+	void addMultipleInputTest() {
+		StringCalculator stringCalculator = new StringCalculator();
+		String input = "1,2";
+		int sum = stringCalculator.add(input);
+		assertEquals(sum, 3);
 	}
 
 }
