090c9a70b32e6961296b2bc49b25a146e2ae9c76
maxsteel1996 <yusufmax3@gmail.com>
2020-06-11 11:41:58 +0530

    initial commit

---
+++ src/com/calculator/test/CalculatorTest.java
@@ -0,0 +1,14 @@
+package com.calculator.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
